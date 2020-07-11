a96af8f92693ef43ff851662f6d3576ac9f03cdc
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-11 23:34:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
-# require modules here
-
-def load_library
-  # code goes here
+ # require modules here
+require 'yaml'
+  
+def load_library(hash)
+  emoticon = yaml.load_file(hash)
+  emoticon.each do |a|
+    print "#{a[0]}"
+    print "#{a[1][0]}"
+    print "#{a[1][1]}"
+  end
 end
 
 def get_japanese_emoticon
